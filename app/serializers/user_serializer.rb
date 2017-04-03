9796a694fdc7da8b766a02dbbd8dc26d3bf2f73c
Nick Doiron <ndoiron@mapmeld.com>
2017-04-02 22:09:55 -0400

    user sign-in add role

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :created_at, :username
+  attributes :id, :email, :created_at, :username, :role
 
   has_many :favorite_posts
   has_many :posts
+  has_many :farmers
 end
